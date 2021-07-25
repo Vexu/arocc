5c0b264ca05d0dffb85010b54ad008d9dab1e346
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2021-07-25 10:34:41 +0100

    Expose parser in lib.zig

--- src/lib.zig
+++ src/lib.zig
@@ -2,3 +2,4 @@ pub const Compilation = @import("Compilation.zig");
 pub const Source = @import("Source.zig");
 pub const Preprocessor = @import("Preprocessor.zig");
 pub const Tokenizer = @import("Tokenizer.zig");
+pub const Parser = @import("Parser.zig");
