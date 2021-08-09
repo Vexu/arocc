45be4d58d3c330be1a00b7503d24d4023ba196c0
Veikka Tuominen <git@vexu.eu>
2021-08-09 14:47:42 +0300

    tests: use std.Progress in test runner

--- test/runner.zig
+++ test/runner.zig
@@ -8,7 +8,7 @@ const predefined_macros =
 
 var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
 
-pub fn main() !u8 {
+pub fn main() !void {
     const gpa = &general_purpose_allocator.allocator;
     defer _ = general_purpose_allocator.deinit();
 
@@ -20,8 +20,35 @@ pub fn main() !u8 {
         return error.InvalidArguments;
     }
 
-    var cases_dir = try std.fs.cwd().openDir(args[1], .{ .iterate = true });
-    defer cases_dir.close();
+    var path_buf = std.ArrayList(u8).init(gpa);
+    defer path_buf.deinit();
+    var cases = std.ArrayList(struct { start: usize, end: usize }).init(gpa);
+    defer cases.deinit();
+
+    // collect all cases
+    {
+        var cases_dir = try std.fs.cwd().openDir(args[1], .{ .iterate = true });
+        defer cases_dir.close();
+
+        var it = cases_dir.iterate();
+        while (try it.next()) |entry| {
+            if (entry.kind != .File) {
+                print("skipping non file entry '{s}'\n", .{entry.name});
+                continue;
+            }
+
+            const start = path_buf.items.len;
+            try path_buf.writer().print("{s}{c}{s}", .{ args[1], std.fs.path.sep, entry.name });
+
+            try cases.append(.{ .start = start, .end = path_buf.items.len });
+        }
+    }
+
+    var progress = std.Progress{};
+    const root_node = progress.start("Test", cases.items.len) catch |err| switch (err) {
+        // TODO still run tests in this case
+        error.TimerUnsupported => @panic("timer unsupported"),
+    };
 
     // prepare compiler
     var comp = aro.Compilation.init(gpa);
@@ -44,25 +71,14 @@ pub fn main() !u8 {
         break :blk source;
     };
 
-    var path_buf = std.ArrayList(u8).init(gpa);
-    defer path_buf.deinit();
-
     // iterate over all cases
-    var any_error = false;
-    var it = cases_dir.iterate();
-    while (try it.next()) |entry| {
-        if (entry.kind != .File) {
-            print("skipping non file entry '{s}'\n", .{entry.name});
-            continue;
-        }
-
-        path_buf.items.len = 0;
-        try path_buf.writer().print("{s}{c}{s}", .{ args[1], std.fs.path.sep, entry.name });
-        const path = path_buf.items;
-
+    var ok_count: u32 = 0;
+    var fail_count: u32 = 0;
+    for (cases.items) |range| {
+        const path = path_buf.items[range.start..range.end];
         const file = comp.addSource(path) catch |err| {
-            any_error = true;
-            print("could not add source '{s}': {s}\n", .{ path, @errorName(err) });
+            fail_count += 1;
+            progress.log("could not add source '{s}': {s}\n", .{ path, @errorName(err) });
             continue;
         };
         defer {
@@ -71,14 +87,21 @@ pub fn main() !u8 {
             gpa.free(file.buf);
         }
 
+        const case = std.mem.sliceTo(std.fs.path.basename(path), '.');
+        var case_node = root_node.start(case, 0);
+        case_node.activate();
+        progress.refresh();
+
+        comp.diag.errors = 0;
         var pp = aro.Preprocessor.init(&comp);
         defer pp.deinit();
 
         try pp.preprocess(builtin_macros);
         try pp.preprocess(test_runner_macros);
         pp.preprocess(file) catch |err| {
-            any_error = true;
-            print("could not preprocess file '{s}': {s}\n", .{ path, @errorName(err) });
+            fail_count += 1;
+            case_node.end();
+            progress.log("could not preprocess file '{s}': {s}\n", .{ path, @errorName(err) });
             continue;
         };
         try pp.tokens.append(pp.comp.gpa, .{
@@ -86,7 +109,6 @@ pub fn main() !u8 {
             .loc = .{ .id = file.id, .byte_offset = @intCast(u32, file.buf.len) },
         });
 
-        const case = std.mem.sliceTo(entry.name, '.');
         if (pp.defines.get("EXPECTED_TOKENS")) |macro| {
             comp.renderErrors();
 
@@ -94,16 +116,18 @@ pub fn main() !u8 {
                 .simple => |simple| simple.tokens,
                 .empty => &[_]aro.Tokenizer.Token{},
                 else => {
-                    any_error = true;
-                    print("case '{s}': invalid EXPECTED_TOKENS {}\n", .{ case, macro });
+                    fail_count += 1;
+                    case_node.end();
+                    progress.log("{s}: invalid EXPECTED_TOKENS {}\n", .{ case, macro });
                     continue;
                 },
             };
 
             if (pp.tokens.len - 1 != expected_tokens.len) {
-                any_error = true;
-                print(
-                    "case '{s}': EXPECTED_TOKENS count differs: expected {d} found {d}\n",
+                fail_count += 1;
+                case_node.end();
+                progress.log(
+                    "{s}: EXPECTED_TOKENS count differs: expected {d} found {d}\n",
                     .{ case, expected_tokens.len, pp.tokens.len - 1 },
                 );
                 continue;
@@ -112,13 +136,19 @@ pub fn main() !u8 {
             var i: usize = 0;
             while (true) : (i += 1) {
                 const tok = pp.tokens.get(i);
-                if (tok.id == .eof) break;
+                if (tok.id == .eof) {
+                    if (comp.diag.errors != 0) fail_count += 1 else ok_count += 1;
+                    case_node.end();
+                    break;
+                }
 
                 const expected = pp.tokSliceSafe(expected_tokens[i]);
                 const actual = pp.expandedSlice(tok);
                 if (!std.mem.eql(u8, expected, actual)) {
-                    print(
-                        "case '{s}': unexpected token found: expected '{s}' found '{s}'\n",
+                    fail_count += 1;
+                    case_node.end();
+                    progress.log(
+                        "{s}: unexpected token found: expected '{s}' found '{s}'\n",
                         .{ case, expected, actual },
                     );
                     break;
@@ -132,11 +162,19 @@ pub fn main() !u8 {
 
         if (pp.defines.get("EXPECTED_ERRORS")) |_| {
             // TODO
+            ok_count += 1;
             continue;
         }
 
         comp.renderErrors();
+        if (comp.diag.errors != 0) fail_count += 1 else ok_count += 1;
     }
 
-    return @boolToInt(any_error or comp.diag.errors != 0);
+    root_node.end();
+    if (ok_count == cases.items.len) {
+        print("All {d} tests passed.\n\n", .{ok_count});
+    } else {
+        print("{d} passed; {d} failed.\n\n", .{ ok_count, fail_count });
+        std.process.exit(1);
+    }
 }
