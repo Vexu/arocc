adc4dfc09897e3a56b97a561dc8635985276c336
Evan Haas <evan@lagerdata.com>
2021-11-04 14:52:44 -0700

    Testing: Add fuzz testing build target
    Adds fuzz testing via AFL++
    
    Code and instructions adapted from https://github.com/squeek502/zig-fuzzing-example

--- build.zig
+++ build.zig
@@ -1,7 +1,40 @@
 const std = @import("std");
 const Builder = std.build.Builder;
 
-pub fn build(b: *Builder) void {
+fn addFuzzStep(b: *Builder) !void {
+    const fuzz_lib = b.addStaticLibrary("fuzz-lib", "test/fuzz.zig");
+    fuzz_lib.addPackagePath("aro", "src/lib.zig");
+    fuzz_lib.setBuildMode(.Debug);
+    fuzz_lib.want_lto = true;
+    fuzz_lib.bundle_compiler_rt = true;
+
+    const fuzz_executable_name = "fuzz";
+    const fuzz_exe_path = try std.fs.path.join(b.allocator, &.{ b.cache_root, fuzz_executable_name });
+
+    // We want `afl-clang-lto -o path/to/output path/to/library`
+    const fuzz_compile = b.addSystemCommand(&.{ "afl-clang-lto", "-o", fuzz_exe_path });
+    // Add the path to the library file to afl-clang-lto's args
+    fuzz_compile.addArtifactArg(fuzz_lib);
+
+    // Install the cached output to the install 'bin' path
+    const fuzz_install = b.addInstallBinFile(.{ .path = fuzz_exe_path }, fuzz_executable_name);
+
+    // Add a top-level step that compiles and installs the fuzz executable
+    const fuzz_compile_run = b.step("fuzz", "Build executable for fuzz testing using afl-clang-lto");
+    fuzz_compile_run.dependOn(&fuzz_compile.step);
+    fuzz_compile_run.dependOn(&fuzz_install.step);
+
+    // Compile a companion exe for debugging crashes
+    const fuzz_debug_exe = b.addExecutable("fuzz-debug", "test/fuzz.zig");
+    fuzz_debug_exe.addPackagePath("aro", "src/lib.zig");
+    fuzz_debug_exe.setBuildMode(.Debug);
+
+    // Only install fuzz-debug when the fuzz step is run
+    const install_fuzz_debug_exe = b.addInstallArtifact(fuzz_debug_exe);
+    fuzz_compile_run.dependOn(&install_fuzz_debug_exe.step);
+}
+
+pub fn build(b: *Builder) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -50,4 +83,6 @@ pub fn build(b: *Builder) void {
     integration_test_runner.addArg(b.pathFromRoot("test/cases"));
     integration_test_runner.addArg(b.zig_exe);
     tests_step.dependOn(&integration_test_runner.step);
+
+    try addFuzzStep(b);
 }
