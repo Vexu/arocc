e3446ee8fbfa81849560d135e87e5f79d01c7504
Evan Haas <evan@lagerdata.com>
2021-11-13 07:41:54 -0800

    Fuzzer: improve performance
    * Use persistent mode for fuzzing - gives a 2-3x speedup
    * use nullwriter for AST dump and omit diagnostic rendering
    * reorganize file locations and remove debug-helper - We can just use the compiler
    	itself when trying to debug crashes
    * manually remove fuzz input source so we don't need to dupe it

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const Builder = std.build.Builder;
 
 fn addFuzzStep(b: *Builder) !void {
-    const fuzz_lib = b.addStaticLibrary("fuzz-lib", "test/fuzz.zig");
+    const fuzz_lib = b.addStaticLibrary("fuzz-lib", "test/fuzz/fuzz_lib.zig");
     fuzz_lib.addPackagePath("aro", "src/lib.zig");
     fuzz_lib.setBuildMode(.Debug);
     fuzz_lib.want_lto = true;
@@ -11,8 +11,8 @@ fn addFuzzStep(b: *Builder) !void {
     const fuzz_executable_name = "fuzz";
     const fuzz_exe_path = try std.fs.path.join(b.allocator, &.{ b.cache_root, fuzz_executable_name });
 
-    // We want `afl-clang-lto -o path/to/output path/to/library`
-    const fuzz_compile = b.addSystemCommand(&.{ "afl-clang-lto", "-o", fuzz_exe_path });
+    // We want `afl-clang-lto -o path/to/output test/fuzz/main.c path/to/library`
+    const fuzz_compile = b.addSystemCommand(&.{ "afl-clang-lto", "-o", fuzz_exe_path, "test/fuzz/main.c" });
     // Add the path to the library file to afl-clang-lto's args
     fuzz_compile.addArtifactArg(fuzz_lib);
 
@@ -23,15 +23,6 @@ fn addFuzzStep(b: *Builder) !void {
     const fuzz_compile_run = b.step("fuzz", "Build executable for fuzz testing using afl-clang-lto");
     fuzz_compile_run.dependOn(&fuzz_compile.step);
     fuzz_compile_run.dependOn(&fuzz_install.step);
-
-    // Compile a companion exe for debugging crashes
-    const fuzz_debug_exe = b.addExecutable("fuzz-debug", "test/fuzz.zig");
-    fuzz_debug_exe.addPackagePath("aro", "src/lib.zig");
-    fuzz_debug_exe.setBuildMode(.Debug);
-
-    // Only install fuzz-debug when the fuzz step is run
-    const install_fuzz_debug_exe = b.addInstallArtifact(fuzz_debug_exe);
-    fuzz_compile_run.dependOn(&install_fuzz_debug_exe.step);
 }
 
 pub fn build(b: *Builder) !void {
---
+++ test/fuzz/fuzz_lib.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+const aro = @import("aro");
+const mem = std.mem;
+const Allocator = mem.Allocator;
+const process = std.process;
+const Codegen = aro.Codegen;
+const Compilation = aro.Compilation;
+const Source = aro.Source;
+const Preprocessor = aro.Preprocessor;
+const Parser = aro.Parser;
+
+var fixed_buffer_mem: [10 * 1024 * 1024]u8 = undefined;
+
+export fn compile_c_buf(buf: [*]const u8, len: c_int) void {
+    compileSlice(buf[0..@intCast(usize, len)]) catch unreachable;
+}
+
+fn compileSlice(buf: []const u8) !void {
+    var fixed_allocator = std.heap.FixedBufferAllocator.init(fixed_buffer_mem[0..]);
+    const allocator = &fixed_allocator.allocator;
+
+    var comp = Compilation.init(allocator);
+    defer comp.deinit();
+
+    try comp.addDefaultPragmaHandlers();
+    try comp.defineSystemIncludes();
+
+    const builtin = try comp.generateBuiltinMacros();
+    const user_source = Source{
+        .id = @intToEnum(Source.Id, comp.sources.count() + 2),
+        .path = "<STDIN>",
+        .buf = buf,
+    };
+    try comp.sources.put(user_source.path, user_source);
+
+    processSource(&comp, builtin, user_source) catch |e| switch (e) {
+        error.FatalError => {},
+        else => |err| return err,
+    };
+    _ = comp.sources.swapRemove(user_source.path);
+}
+
+fn processSource(comp: *Compilation, builtin: Source, user_source: Source) !void {
+    var pp = Preprocessor.init(comp);
+    defer pp.deinit();
+    try pp.addBuiltinMacros();
+
+    try pp.preprocess(builtin);
+    try pp.preprocess(user_source);
+    try pp.tokens.append(pp.comp.gpa, .{
+        .id = .eof,
+        .loc = .{ .id = user_source.id, .byte_offset = @intCast(u32, user_source.buf.len) },
+    });
+
+    var tree = try Parser.parse(&pp);
+    defer tree.deinit();
+
+    try tree.dump(std.io.null_writer);
+}
