6951e8aac4a6c6146f4d64cc7f53ffbfecd0a4aa
Evan Haas <evan@lagerdata.com>
2021-11-13 20:49:29 -0800

    Parser: universal character name fixes
    1. handle u21 overflow when parsing 8-digit escapes
    2. don't try to encode surrogate code points
    
    Fixes #90

--- test/cases/strings.c
+++ test/cases/strings.c
@@ -6,8 +6,15 @@ _Static_assert(1, "\u0062");
 
 int a = 'abcde';
 
+_Static_assert(1, "\uD800");
+_Static_assert(1, "\U0000DFFF");
+_Static_assert(1, "\UFFFFFFFF");
+
 #define EXPECTED_ERRORS "strings.c:2:30: error: escape sequence out of range" \
     "strings.c:4:20: error: invalid universal character" \
     "strings.c:5:20: error: invalid universal character" \
     "strings.c:7:9: warning: multi-character character constant" \
-    "strings.c:7:9: warning: character constant too long for its type"
+    "strings.c:7:9: warning: character constant too long for its type" \
+    "strings.c:9:20: error: invalid universal character" \
+    "strings.c:10:20: error: invalid universal character" \
+    "strings.c:11:20: error: invalid universal character" \
