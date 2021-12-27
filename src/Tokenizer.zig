807e69b387ef6d07ad427ef1fdba748a016735bd
Veikka Tuominen <git@vexu.eu>
2021-12-27 19:00:17 +0200

    Preprocessor: implement __has_builtin

--- src/Tokenizer.zig
+++ src/Tokenizer.zig
@@ -130,6 +130,8 @@ pub const Token = struct {
         macro_param_has_feature,
         /// Special token for implementing __has_extension
         macro_param_has_extension,
+        /// Special token for implementing __has_builtin
+        macro_param_has_builtin,
         /// Special token for implementing __is_identifier
         macro_param_is_identifier,
         /// Special token for implementing __FILE__
@@ -389,6 +391,7 @@ pub const Token = struct {
                 .macro_param_has_warning,
                 .macro_param_has_feature,
                 .macro_param_has_extension,
+                .macro_param_has_builtin,
                 .macro_param_is_identifier,
                 .macro_file,
                 .macro_line,
