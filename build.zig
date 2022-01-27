a52124029dd0b14245a47b57655d68078142fc8b
Veikka Tuominen <git@vexu.eu>
2022-01-27 19:20:55 +0200

    build.zig: ignore exit code of run step
    It is very much expected for the compiler to either 1 or 0.

--- build.zig
+++ build.zig
@@ -49,6 +49,7 @@ pub fn build(b: *Builder) !void {
     exe.install();
 
     const run_cmd = exe.run();
+    run_cmd.expected_exit_code = null;
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
