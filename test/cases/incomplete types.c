b1ef60c28801b09a450d372fa76f8452ea880e3c
Evan Haas <evan@lagerdata.com>
2022-03-08 23:59:47 -0800

    Parser: handle incomplete-size types used in switch statement
    Fixes #256

--- test/cases/incomplete types.c
+++ test/cases/incomplete types.c
@@ -29,6 +29,13 @@ void contains_self_referential(void) {
     struct self_referential s = {0};
 }
 
+void switch_with_incomplete(void) {
+    struct node n;
+    switch (n) {
+        case 1: break;
+    }
+}
+
 #define EXPECTED_ERRORS "incomplete types.c:4:5: error: dereferencing pointer to incomplete type 'struct S'" \
     "incomplete types.c:5:11: error: dereferencing pointer to incomplete type 'struct S'" \
     "incomplete types.c:8:5: error: dereferencing pointer to incomplete type 'union U'" \
@@ -38,3 +45,5 @@ void contains_self_referential(void) {
     "incomplete types.c:21:17: error: variable has incomplete type 'struct node'" \
     "incomplete types.c:25:29: error: field has incomplete type 'struct self_referential'" \
     "incomplete types.c:29:29: error: variable has incomplete type 'struct self_referential'" \
+    "incomplete types.c:33:17: error: variable has incomplete type 'struct node'" \
+    "incomplete types.c:34:13: error: statement requires expression with integer type ('struct node' invalid)" \
