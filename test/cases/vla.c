6e4771cb482736f86a42c1c8299760a720d76307
Evan Haas <evan@lagerdata.com>
2022-04-01 23:14:50 -0700

    Type: Fix sizeof calculation for array of incomplete-size items
    Fixes #268

--- test/cases/vla.c
+++ test/cases/vla.c
@@ -8,5 +8,10 @@ struct S {
     int y[x];
 };
 
+void bar(int x) {
+    int arr[2][3][x];
+}
+
 #define EXPECTED_ERRORS "vla.c:3:13: warning: variable length array used [-Wvla]" \
     "vla.c:8:11: error: use of undeclared identifier 'x'" \
+    "vla.c:12:19: warning: variable length array used [-Wvla]" \
