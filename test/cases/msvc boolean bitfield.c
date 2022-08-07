867ffc855cd00dd85f70f662bdb2e597d4c0e45a
Evan Haas <evan@lagerdata.com>
2022-08-07 08:44:21 -0700

    Type: _Bool on MSVC is 8 bits
    Closes #363

---
+++ test/cases/msvc boolean bitfield.c
@@ -0,0 +1,12 @@
+//aro-args --target=x86_64-windows-msvc
+
+typedef struct {
+    _Bool a: 4;
+    _Bool : 2;
+    _Bool c: 2;
+} S;
+
+// _Static_assert(sizeof(S) == 1, "");
+_Static_assert(_Alignof(S) == 1, "");
+
+#define TESTS_SKIPPED 1
