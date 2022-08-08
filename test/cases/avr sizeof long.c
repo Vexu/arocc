0f97c9644e58544978ea3d49f7640e22f7555f12
Evan Haas <evan@lagerdata.com>
2022-08-07 19:59:01 -0700

    Zig dependency: update type.zig
    Fixes #366

---
+++ test/cases/avr sizeof long.c
@@ -0,0 +1,11 @@
+//aro-args --target=avr-freestanding-none
+
+_Static_assert(sizeof(int) == 2, "wrong sizeof int");
+_Static_assert(sizeof(unsigned) == 2, "wrong sizeof unsigned");
+_Static_assert(sizeof(short) == 2, "wrong sizeof short");
+_Static_assert(sizeof(unsigned short) == 2, "wrong sizeof unsigned short");
+_Static_assert(sizeof(long) == 4, "wrong sizeof long");
+_Static_assert(sizeof(unsigned long) == 4, "wrong sizeof unsigned long");
+_Static_assert(sizeof(long double) == 4, "wrong sizeof long double");
+_Static_assert(sizeof(long long) == 8, "wrong sizeof long long");
+_Static_assert(sizeof(unsigned long long) == 8, "wrong sizeof unsigned long long");
