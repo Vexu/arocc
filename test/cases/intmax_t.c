2f313bdc3667e5218b266ba34bb3604de9bace03
Evan Haas <evan@lagerdata.com>
2023-05-08 13:48:29 -0700

    Compilation: generate MAX macro values correctly for unsigned types

---
+++ test/cases/intmax_t.c
@@ -0,0 +1,7 @@
+//aro-args --target=x86_64-linux-gnu
+
+_Static_assert(__INTMAX_MAX__ == 9223372036854775807L, "");
+_Static_assert(__INTMAX_WIDTH__ == 64, "");
+
+_Static_assert(__UINTMAX_MAX__ == 18446744073709551615UL, "");
+_Static_assert(__UINTMAX_WIDTH__ == 64, "");
