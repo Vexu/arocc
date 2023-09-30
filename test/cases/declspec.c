05bedcb3316e82cfd3fdd026d56d354f7806f9ff
bing <bingcicle@proton.me>
2023-10-01 03:08:53 +0800

    feat: implement `__has_declspec_attribute`
    * __has_declspec_attribute always 0 if not enabled in opts

--- test/cases/declspec.c
+++ test/cases/declspec.c
@@ -8,20 +8,18 @@ __declspec(aligned(16)) int baz;
 
 _Static_assert(_Alignof(bar) == 16, "wrong alignment");
 
-// #if __has_declspec_attribute(foo)
-// #error fail
-// #endif
+#if __has_declspec_attribute(foo)
+#error fail
+#endif
 
-// #if !__has_declspec_attribute(align)
-// #error fail
-// #endif
+#if !__has_declspec_attribute(align)
+#error fail
+#endif
 
 typedef int Int1 __declspec(align(8));
 typedef __declspec(align(8)) int Int2;
 
 _Static_assert(_Alignof(Int2) == 8, "");
-
-#define TESTS_SKIPPED 2
 
 #define EXPECTED_ERRORS "declspec.c:7:12: warning: __declspec attribute 'aligned' is not supported [-Wignored-attributes]" \
 	"declspec.c:19:18: error: 'declspec' attribute not allowed after declarator" \
--- test/cases/no declspec.c
+++ test/cases/no declspec.c
@@ -1,4 +1,12 @@
 __declspec(align(4)) int foo;
 
+#if __has_declspec_attribute(noreturn)
+#error "__has_declspec_attribute macro should not work without -fdeclspec"
+#endif
+
+#if __has_declspec_attribute(x)
+#error "__has_declspec_attribute macro should not work without -fdeclspec"
+#endif
+
 #define EXPECTED_ERRORS \
     "no declspec.c:1:1: error: '__declspec' attributes are not enabled; use '-fdeclspec' or '-fms-extensions' to enable support for __declspec attributes" \
