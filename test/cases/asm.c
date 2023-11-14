9936c2bdf80ad8b3ba8af8d65735245ea807b69e
Evan Haas <evan@lagerdata.com>
2023-11-13 19:43:49 -0800

    Parser: ensure that asmStr finds at least one string literal
    Parser.stringLiteral assumes the first token it sees will be
    some sort of string literal

--- test/cases/asm.c
+++ test/cases/asm.c
@@ -2,9 +2,12 @@ void foo(void) __asm__("foo");
 __asm__ volatile volatile("foo");
 __asm__(u8"nop");
 __asm__(L"nop");
+__asm__(5);
 
 #define EXPECTED_ERRORS "asm.c:2:9: error: meaningless 'volatile' on assembly outside function" \
     "asm.c:2:18: error: meaningless 'volatile' on assembly outside function" \
     "asm.c:2:18: error: duplicate asm qualifier 'volatile'" \
     "asm.c:3:9: error: cannot use unicode string literal in assembly" \
     "asm.c:4:9: error: cannot use wide string literal in assembly" \
+    "asm.c:5:9: error: expected string literal in 'asm'" \
+
