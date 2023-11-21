7c02f655928b9bb074701710b1f5f52ca059cb21
Veikka Tuominen <git@vexu.eu>
2023-11-21 18:16:08 +0200

    Tokenizer: fix missing symbols for some tokens
    Closes #575

--- test/cases/_BitInt.c
+++ test/cases/_BitInt.c
@@ -19,6 +19,7 @@ unsigned _BitInt(5) h = 3Uwb;
 #pragma GCC diagnostic ignored "-Wc23-extensions"
 int y = 0wb;
 int z = 0uwb;
+int x = 1'2;
 
 #define EXPECTED_ERRORS "_BitInt.c:3:1: warning: '_BitInt' in C17 and earlier is a Clang extension' [-Wbit-int-extension]" \
     "_BitInt.c:13:1: error: _BitInt of bit sizes greater than 65535 not supported" \
@@ -26,4 +27,5 @@ int z = 0uwb;
     "_BitInt.c:15:10: error: unsigned _BitInt must have a bit size of at least 1" \
     "_BitInt.c:17:16: warning: '_BitInt' suffix for literals is a C23 extension [-Wc23-extensions]" \
     "_BitInt.c:18:25: warning: '_BitInt' suffix for literals is a C23 extension [-Wc23-extensions]" \
-
+    "_BitInt.c:18:25: warning: '_BitInt' suffix for literals is a C23 extension [-Wc23-extensions]" \
+    "_BitInt.c:22:10: error: expected ';', found 'a character literal'" \
