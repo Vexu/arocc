5d93208f12958ff501c671bc10b4970f71038613
bing <bingcicle@proton.me>
2023-11-30 20:27:10 +0800

    add one more test for mul

--- test/cases/arithmetic overflow.c
+++ test/cases/arithmetic overflow.c
@@ -3,3 +3,4 @@ _Static_assert(1 + 0xFFFFFFFF == 0, "'");
 _Static_assert(0 - 0x00000001 == 0xFFFFFFFF, "'");
 _Static_assert(0x00000000 - 0x00000001 == 0xFFFFFFFF, "'");
 _Static_assert(0x80000000 * 2 == 0, "'");
+_Static_assert(2 * 0x80000000 == 0, "'");
