28ca8cadb5433bf7355c090e3e3de16ecfee0f0a
Evan Haas <evan@lagerdata.com>
2024-04-07 14:58:45 -0700

    Parser: don't concatenate improperly-encoded strings
    Fixes #674

---
+++ test/cases/concat improperly encoded strings.c
@@ -0,0 +1,5 @@
+/* Note: saved with iso-8859-1 encoding, do not change */
+int *p = L"" "ÿ";
+
+#define EXPECTED_ERRORS "concat improperly encoded strings.c:2:14: error: illegal character encoding in character literal" \
+
