8a225f1e22052e6f80b9f39146ce555f8478fb95
Evan Haas <evan@lagerdata.com>
2024-04-15 21:01:27 -0700

    Type: Improve type comparison of fixed-type enums
    Enums types with fixed underlying type should compare equal to the
    underlying type, but not to other enums with the same fixed underlying
    type.
    
    Closes #683

--- test/cases/enum fixed.c
+++ test/cases/enum fixed.c
@@ -34,6 +34,30 @@ enum E e;
 enum E: int;
 void fn(enum E);
 
+enum Signed: int {
+    S,
+};
+
+enum Signed2: int {
+    S2,
+};
+
+enum Unsigned: unsigned {
+    U,
+};
+
+enum Plain {
+    P,
+};
+
+_Static_assert(!__builtin_types_compatible_p(enum Signed, enum Signed2), "");
+_Static_assert(!__builtin_types_compatible_p(enum Signed, enum Unsigned), "");
+_Static_assert(__builtin_types_compatible_p(enum Signed, int), "");
+_Static_assert(__builtin_types_compatible_p(enum Unsigned, unsigned), "");
+
+_Static_assert(!__builtin_types_compatible_p(enum Signed, enum Plain), "");
+_Static_assert(!__builtin_types_compatible_p(enum Unsigned, enum Plain), "");
+
 #define EXPECTED_ERRORS "enum fixed.c:2:7: warning: enumeration types with a fixed underlying type are a Clang extension [-Wfixed-enum-extension]" \
     "enum fixed.c:4:6: error: enumeration previously declared with fixed underlying type" \
     "enum fixed.c:2:6: note: previous definition is here" \
