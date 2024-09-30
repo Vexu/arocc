71330d9836f3f0addf721503135f5db5e3b4d1b2
Богдан Романюк <wrongnull@gmail.com>
2024-09-30 23:49:08 +0300

    update integration test

--- test/cases/gnu pointer arith.c
+++ test/cases/gnu pointer arith.c
@@ -1,8 +1,19 @@
 //aro-args -Wgnu-pointer-arith
 
-#include <stddef.h>
-ptrdiff_t foo(void *a, void *b) {
-    return b - a;
+void foo(void *a, void *b) {
+    b - a;
+    a - 1;
+    a + 1;
+    ++a;
+    --a;
+    b++;
+    b--;
 }
 
-#define EXPECTED_ERRORS "gnu pointer arith.c:5:14: warning: arithmetic on pointers to void is a GNU extension [-Wgnu-pointer-arith]"
+#define EXPECTED_ERRORS "gnu pointer arith.c:4:7: warning: arithmetic on pointers to void is a GNU extension [-Wgnu-pointer-arith]"\
+                        "gnu pointer arith.c:5:7: warning: arithmetic on pointers to void is a GNU extension [-Wgnu-pointer-arith]"\
+                        "gnu pointer arith.c:6:7: warning: arithmetic on pointers to void is a GNU extension [-Wgnu-pointer-arith]"\
+                        "gnu pointer arith.c:7:5: warning: arithmetic on pointers to void is a GNU extension [-Wgnu-pointer-arith]"\
+                        "gnu pointer arith.c:8:5: warning: arithmetic on pointers to void is a GNU extension [-Wgnu-pointer-arith]"\
+                        "gnu pointer arith.c:9:6: warning: arithmetic on pointers to void is a GNU extension [-Wgnu-pointer-arith]"\
+                        "gnu pointer arith.c:10:6: warning: arithmetic on pointers to void is a GNU extension [-Wgnu-pointer-arith]"
