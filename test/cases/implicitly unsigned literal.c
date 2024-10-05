7ed4cb0896d8d05ef7b3f7d6337e08ac05759d5d
Evan Haas <evan@lagerdata.com>
2024-10-05 13:47:02 -0700

    Parser: only warn for implicitly-unsigned integer literals for base 10

--- test/cases/implicitly unsigned literal.c
+++ test/cases/implicitly unsigned literal.c
@@ -1,5 +1,6 @@
 _Static_assert(-9223372036854775808LL > 0, "lhs should be unsigned");
 long long x = 9223372036854775808LL;
+_Static_assert(0xFFFFFFFFFFFFFFFF == 0xFFFFFFFFFFFFFFFE + 1, "");
 
 #define EXPECTED_ERRORS \
 	"implicitly unsigned literal.c:1:17: warning: integer literal is too large to be represented in a signed integer type, interpreting as unsigned [-Wimplicitly-unsigned-literal]" \
