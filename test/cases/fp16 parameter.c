1e1104cd8f3acd722e9aae8a49abe1502ada761f
Veikka Tuominen <git@vexu.eu>
2024-10-09 11:43:42 +0300

    tests: fix test case missing target

--- test/cases/fp16 parameter.c
+++ test/cases/fp16 parameter.c
@@ -1,7 +1,8 @@
+//aro-args --target=x86_64-linux-gnu
 __fp16 foo(__fp16 param) {
     return 0;
 }
 
-#define EXPECTED_ERRORS "fp16 parameter.c:1:19: error: parameters cannot have __fp16 type; did you forget * ?" \
-    "fp16 parameter.c:1:11: error: function return value cannot have __fp16 type; did you forget * ?" \
+#define EXPECTED_ERRORS "fp16 parameter.c:2:19: error: parameters cannot have __fp16 type; did you forget * ?" \
+    "fp16 parameter.c:2:11: error: function return value cannot have __fp16 type; did you forget * ?" \
 
