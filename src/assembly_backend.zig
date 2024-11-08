ad254c1038fee111aba9ca3eaa2edac37128ce8a
Evan Haas <evan@lagerdata.com>
2024-11-08 13:04:10 -0800

    Driver: decouple assembly codegen from frontend in a cleaner way

--- src/assembly_backend.zig
+++ src/assembly_backend.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
 
 const aro = @import("aro");
-pub const Error = aro.Compilation.Error || error{CodegenFailed};
 
 pub const x86_64 = @import("assembly_backend/x86_64.zig");
 
-pub fn genAsm(target: std.Target, tree: aro.Tree) Error!aro.Assembly {
+pub fn genAsm(target: std.Target, tree: aro.Tree) aro.Compilation.Error!aro.Assembly {
     return switch (target.cpu.arch) {
         .x86_64 => x86_64.genAsm(tree),
         else => std.debug.panic("genAsm not implemented: {s}", .{@tagName(target.cpu.arch)}),
