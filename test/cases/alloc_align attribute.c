a88c5ac304702aea927f4a987b6dea9480e6d4c4
Evan Haas <evan@lagerdata.com>
2024-12-04 11:30:45 -0800

    Attribute: implement alloc_align

---
+++ test/cases/alloc_align attribute.c
@@ -0,0 +1,12 @@
+__attribute__((alloc_align(0))) int* foo1(int x, double y);
+__attribute__((alloc_align(1))) int* foo2(int x, double y);
+__attribute__((alloc_align(2))) int* foo3(int x, double y);
+__attribute__((alloc_align(3))) int* foo4(int x, double y);
+__attribute__((alloc_align(1))) int foo5(int x, double y);
+
+#define EXPECTED_ERRORS \
+    "alloc_align attribute.c:1:16: error: 'alloc_align' attribute parameter 1 is out of bounds" \
+    "alloc_align attribute.c:3:16: error: 'alloc_align' attribute argument may only refer to a function parameter of integer type" \
+    "alloc_align attribute.c:4:16: error: 'alloc_align' attribute parameter 1 is out of bounds" \
+    "alloc_align attribute.c:5:16: warning: 'alloc_align' attribute only applies to return values that are pointers [-Wignored-attributes]" \
+
