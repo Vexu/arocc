2963249923b46928f550423188528ccbd70334bb
Evan Haas <evan@lagerdata.com>
2024-12-06 19:55:07 -0800

    zig update: remove bridgeos

--- src/aro/target.zig
+++ src/aro/target.zig
@@ -682,7 +682,6 @@ pub fn toLLVMTriple(target: std.Target, buf: []u8) []const u8 {
         .vulkan,
         .plan9,
         .other,
-        .bridgeos,
         => "unknown",
     };
     writer.writeAll(llvm_os) catch unreachable;
