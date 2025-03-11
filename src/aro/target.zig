7e45abe0bf550a881bbb0892dcfc1650f3c4d930
Organ1sm <ywwithpeople@gmail.com>
2025-03-11 16:37:53 +0800

    zig: update the latest zig

--- src/aro/target.zig
+++ src/aro/target.zig
@@ -207,7 +207,7 @@ pub fn unnamedFieldAffectsAlignment(target: std.Target) bool {
         },
         .armeb => {
             if (std.Target.arm.featureSetHas(target.cpu.features, .has_v7)) {
-                if (std.Target.Abi.default(target.cpu.arch, target.os) == .eabi) return true;
+                if (std.Target.Abi.default(target.cpu.arch, target.os.tag) == .eabi) return true;
             }
         },
         .arm => return true,
@@ -953,7 +953,7 @@ pub fn isPICDefaultForced(target: std.Target) DefaultPIStatus {
 test "alignment functions - smoke test" {
     const linux: std.Target.Os = .{ .tag = .linux, .version_range = .{ .none = {} } };
     const x86_64_target: std.Target = .{
-        .abi = std.Target.Abi.default(.x86_64, linux),
+        .abi = std.Target.Abi.default(.x86_64, linux.tag),
         .cpu = std.Target.Cpu.Model.generic(.x86_64).toCpu(.x86_64),
         .os = linux,
         .ofmt = .elf,
@@ -973,7 +973,7 @@ test "target size/align tests" {
 
     const linux: std.Target.Os = .{ .tag = .linux, .version_range = .{ .none = {} } };
     const x86_target: std.Target = .{
-        .abi = std.Target.Abi.default(.x86, linux),
+        .abi = std.Target.Abi.default(.x86, linux.tag),
         .cpu = std.Target.Cpu.Model.generic(.x86).toCpu(.x86),
         .os = linux,
         .ofmt = .elf,
