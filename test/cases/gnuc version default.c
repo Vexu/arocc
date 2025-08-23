39c00641521fb8d4e98681601ec51bf6813a185b
Evan Haas <evan@lagerdata.com>
2025-08-23 09:16:26 -0700

    Compilation: treat __ARO_EMULATE__ similar to __BYTE_ORDER__

--- test/cases/gnuc version default.c
+++ test/cases/gnuc version default.c
@@ -1,20 +1,17 @@
-#if __ARO_EMULATE__ == 1
+#if __ARO_EMULATE__ == __ARO_EMULATE_CLANG__
 
-/* clang */
 _Static_assert(__GNUC__ == 4, "");
 _Static_assert(__GNUC_MINOR__ == 2, "");
 _Static_assert(__GNUC_PATCHLEVEL__ == 1, "");
 
-#elif __ARO_EMULATE__ == 2
+#elif __ARO_EMULATE__ == __ARO_EMULATE_GCC__
 
-/* gcc */
 _Static_assert(__GNUC__ == 7, "");
 _Static_assert(__GNUC_MINOR__ == 1, "");
 _Static_assert(__GNUC_PATCHLEVEL__ == 0, "");
 
-#elif __ARO_EMULATE__ == 3
+#elif __ARO_EMULATE__ == __ARO_EMULATE_MSVC__
 
-/* msvc */
 #if defined(__GNUC__) || defined(__GNUC_MINOR__) || defined(__GNUC_PATCHLEVEL__)
 #error "__GNUC__ macros should not be defined"
 #endif
