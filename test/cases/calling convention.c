0fbfec038ca1373f771f5f16197bd30b53067b12
Evan Haas <evan@lagerdata.com>
2025-11-23 08:52:28 -0800

    Attribute: handle additional ways calling convention can be applied
    Closes #879

---
+++ test/cases/calling convention.c
@@ -0,0 +1,13 @@
+__attribute__((sysv_abi)) void sysv_func(void);
+
+__attribute__((sysv_abi)) int x;
+
+
+typedef __attribute__((sysv_abi)) void (*fn_ptr_t)(void);
+void foo(__attribute__((sysv_abi)) void (*p)(void)) {
+	__attribute__((sysv_abi)) void (*fn_ptr)(void);
+}
+
+
+#define EXPECTED_ERRORS "calling convention.c:3:16: warning: 'sysv_abi' only applies to function types; type here is 'int' [-Wignored-attributes]" \
+
