40253bb9ef7ebe07669ab3e53d2d5b0860d42796
Veikka Tuominen <git@vexu.eu>
2026-02-12 16:09:21 +0200

    remove path separators from test manifests

--- test/cases/clang __BASE_FILE__.c
+++ test/cases/clang __BASE_FILE__.c
@@ -4,5 +4,5 @@
 expand
 args = --emulate=clang -Iinclude -Wpedantic
 
-include/base_file_redirect.h:1:32: warning: __BASE_FILE__ is a clang extension [-Wbase-file-extension]
+base_file_redirect.h:1:32: warning: __BASE_FILE__ is a clang extension [-Wbase-file-extension]
 */
--- test/cases/clang __INCLUDE_LEVEL__.c
+++ test/cases/clang __INCLUDE_LEVEL__.c
@@ -6,6 +6,6 @@ expand
 args = --emulate=clang -Iinclude -Wpedantic
 
 clang __INCLUDE_LEVEL__.c:1:33: warning: __INCLUDE_LEVEL__ is a clang extension
-include/nested_include_1.h:1:34: warning: __INCLUDE_LEVEL__ is a clang extension
-include/nested_include_2.h:1:35: warning: __INCLUDE_LEVEL__ is a clang extension
+nested_include_1.h:1:34: warning: __INCLUDE_LEVEL__ is a clang extension
+nested_include_2.h:1:35: warning: __INCLUDE_LEVEL__ is a clang extension
 */
