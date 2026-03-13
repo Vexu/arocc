a035e8b86298690a09b5a99acd02f09e2feeeae6
Carmen <carmen@dotcarmen.dev>
2026-03-13 12:55:26 +0100

    more review fixes

--- test/cases/block types.c
+++ test/cases/block types.c
@@ -3,6 +3,12 @@ typedef int (^BlockIntParamReturnsInt)(int);
 typedef void (^*PointerToBlockNoParamsReturnsVoid)(void);
 typedef int ^invalidBlock;
 
+typedef int aFunc(int);
+typedef aFunc ^bar;
+
+typedef int MyInt;
+typedef ^MyInt;
+
 void my_func_accepting_block(double (*afunc)(int), double (^ablock)(int)) {
   afunc(1);
   ablock(1);
@@ -12,5 +18,6 @@ void my_func_accepting_block(double (*afunc)(int), double (^ablock)(int)) {
 syntax
 args = -fblocks -Wpedantic --target=aarch64-macos
 
-block types.c:4:26: error: Blocks must be a function type
+block types.c:4:26: error: block pointer to non-function type is invalid
+block types.c:10:10: error: block pointer to non-function type is invalid
 */
