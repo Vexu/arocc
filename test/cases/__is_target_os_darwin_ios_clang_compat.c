b3c45f1d876b2b1f9c56f7a30e4b07f73fdd88c9
Julian Dax <julian.dax@posteo.de>
2026-05-01 22:52:15 +0200

    Target: add support for OS tags like `ios12`
    Also add tests for better clang compat

---
+++ test/cases/__is_target_os_darwin_ios_clang_compat.c
@@ -0,0 +1,14 @@
+// This file is based on https://github.com/llvm/llvm-project/blob/main/clang/test/Preprocessor/is_target_os_darwin.c
+
+#if !__is_target_os(darwin)
+  #error "mismatching os"
+#endif
+
+#if !__is_target_os(ios)
+  #error "mismatching os"
+#endif
+
+/** manifest:
+expand_error
+args = --target=x86_64-apple-ios --emulate=clang
+*/
