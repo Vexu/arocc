ca4207a843e8512b2e5ed737668d20d59967cc62
Evan Haas <evan@lagerdata.com>
2026-05-21 23:32:43 -0700

    tests: more tests for integer suffixes

---
+++ test/cases/clang integer suffixes arm64-linux-gnu.c
@@ -0,0 +1,15 @@
+#include "include/test_helpers.h"
+
+_Static_assert(VALUE_HAS_TYPE(0I8, char), "I8 suffix not correct");
+_Static_assert(VALUE_HAS_TYPE(1UI8, unsigned char), "UI8 suffix not correct");
+_Static_assert(VALUE_HAS_TYPE(2I16, short), "I16 suffix not correct");
+_Static_assert(VALUE_HAS_TYPE(3UI16, unsigned short), "UI16 suffix not correct");
+_Static_assert(VALUE_HAS_TYPE(4I32, int), "I32 suffix not correct");
+_Static_assert(VALUE_HAS_TYPE(5UI32, unsigned int), "UI32 suffix not correct");
+_Static_assert(VALUE_HAS_TYPE(6I64, long), "I64 suffix not correct");
+_Static_assert(VALUE_HAS_TYPE(7UI64, unsigned long), "UI64 suffix not correct");
+
+/** manifest:
+syntax
+args = --target=arm64-linux-gnu -fms-extensions --emulate=clang
+*/
