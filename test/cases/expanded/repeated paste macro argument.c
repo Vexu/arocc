44589d098e9f0054ebc3f62a7b1f4da2a2faabb6
Evan Haas <evan@lagerdata.com>
2026-06-22 23:42:10 -0700

    Preprocessor: don't clear hideset for parameter before ##

---
+++ test/cases/expanded/repeated paste macro argument.c
@@ -0,0 +1,1 @@
+short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b) short(b,b)
